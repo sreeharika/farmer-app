eb3640143e67897a785284b7d8472c1649fd67dc
sreeharika <sreeharikayelle@gmail.com>
2020-05-06 00:19:33 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { Component } from 'react'
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/Login"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/Register"}>Register</Link>
+              </li>
+            </ul>
+          </div>
+      
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route path="/Login" component={Login} />
+            <Route path="/Register" component={Register} />
+          </Switch>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
-
 export default App;
+
+    
+          // <a
+          //   className="App-link"
+          //   href="https://react-redux.js.org/"
+          //   target="_blank"
+          //   rel="noopener noreferrer"
+          // >
+          //   React Redux
+          // </a>
+      
+  
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
+// import store from './app/store';
+// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <BrowserRouter>
+  <App></App>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
