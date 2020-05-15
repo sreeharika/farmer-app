77e641ec46a3275d629d56b4b387f163a4a76afd
vaulstein <vaulstein@gmail.com>
2020-05-15 13:32:20 +0530

    Added CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,79 +5,72 @@ import { loginUser } from "../actions";
 import axios from 'axios';
 
 class Login extends Component {
-    state = {
-      email:'',
-      password:'',
-      isLogged: false,
-      };
-          
-    handleChangeEmail = (event) => {
-      this.setState({
-        email: event.target.value,
-      });
-    };
-    handleChangePassword = (event) => {
-      this.setState({
-        password: event.target.value,
-      });
-    };
-    // handlePasswordChange = ({ event }) => {
-    //     this.setState({ password: event.target.value });
-    //   };
-    
-    handleSubmit = () => {
-        const { dispatch } = this.props;
-        dispatch(loginUser(this.state.email, this.state.password));
+  state = {
+    email: '',
+    password: '',
+    isLogged: false,
+  };
+
+  handleChangeEmail = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+  handleChangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+  // handlePasswordChange = ({ event }) => {
+  //     this.setState({ password: event.target.value });
+  //   };
+
+  handleSubmit = () => {
+    const { dispatch } = this.props;
+    dispatch(loginUser(this.state.email, this.state.password));
+  }
+  render() {
+    const { classes, loginError, isAuthenticated } = this.props;
+    console.log("isAuthenticated: ", isAuthenticated)
+    if (isAuthenticated) {
+      return <Redirect to="/Farmerhome" />
     }
-    render() {
-        const { classes, loginError, isAuthenticated } = this.props;
-        console.log("isAuthenticated: ",isAuthenticated)
-        if (isAuthenticated){
-            return <Redirect to="/Farmerhome" />
-        }
-        return (
-            <div className='loginpage'>
-            
-            <h3 style={{textAlign:"center"}}>Login</h3>
-            <div className="form-group">
-                <label>Email</label>
-                <input type="text" className="form-control" placeholder="Enter email" onChange={this.handleChangeEmail} />
+    return (
+      <div id="bg"></div>
+      <div class="wrapper fadeInDown">
+        <div id="form">
+          <div id="formHeader">
+            <h2 class="active"> Sign In </h2>
+            <h2 class="inactive underlineHover"><Link to={"/Register"}>Sign Up</Link></h2>
+            <div class="fadeIn first">
+              <i class="fa fa-user-circle-o"></i>
             </div>
-
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter Password" onChange={this.handleChangePassword} />
+            <div id="formContent">
+              <form>
+                <input type="text" id="login" class="fadeIn second" name="login" placeholder="Login" onChange={this.handleChangeEmail} />
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password" onChange={this.handleChangePassword} />
+                <input type="submit" class="fadeIn fourth" value="Log In" onClick={this.handleSubmit} />
+              </form>
             </div>
-
-            <div className="form-group">
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                </div>
+            <div id="formFooter">
+              <a class="underlineHover" href="#">Forgot Password?</a>
             </div>
-
-            <button type="submit" className="btn btn-primary btn-block" onClick={this.handleSubmit}>Submit</button>
-            <p className="forgot-password text-right">
-                Forgot <a href="#">password?</a>
-            </p>
-            <p className="New register text-left">
-            Register<Link to={"/Register"}>Here</Link>
-        </p>
-
+          </div>
         </div>
+      </div>
     );
-}
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-      isLoggingIn: state.auth.isLoggingIn,
-      loginError: state.auth.loginError,
-      isAuthenticated: state.auth.isAuthenticated
-    };
-  }
-  export default connect(mapStateToProps)(Login);
-  
+  return {
+    isLoggingIn: state.auth.isLoggingIn,
+    loginError: state.auth.loginError,
+    isAuthenticated: state.auth.isAuthenticated
+  };
+}
+export default connect(mapStateToProps)(Login);
+
 
 
 // export default Login;
